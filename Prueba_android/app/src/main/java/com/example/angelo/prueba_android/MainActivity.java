82cc0e8a98fe3930bd2c7304129869b5b574a0d2
Angelo1793 <angelo1793@hotmail.com>
2017-10-04 17:53:19 -0500

    Capitulo 10: TextView y EditText
    -Creando Texview con mi nombre
    -Creando EditText con texto
    -Crenado EditText con hint
    -Creando String para los nombres
    -Creando las  vistas en main.java para modificarlas

--- Prueba_android/app/src/main/java/com/example/angelo/prueba_android/MainActivity.java
+++ Prueba_android/app/src/main/java/com/example/angelo/prueba_android/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.angelo.prueba_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    EditText etext1,etext2;
+    TextView text;
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            etext1=(EditText) findViewById(R.id.textInput);
+            etext2=(EditText) findViewById(R.id.textInput2);
+            text=(TextView) findViewById(R.id.textName);
     }
 }
